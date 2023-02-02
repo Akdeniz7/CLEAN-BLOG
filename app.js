5ef3182fb81d730c8716e8bfb60f4a2e25afd511
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-02 21:05:49 +0300

    Template and Template engine added

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description"
-  }
-  res.send(blog);
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
