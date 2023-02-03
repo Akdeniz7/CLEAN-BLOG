a113e8082d678c026aee59d814306e2aea2a059f
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-03 18:14:19 +0300

    Database connection established

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
+const Blog = require('./models/BlogDetails');
 
 const app = express();
 
-//TEMPLATE ENGINE
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1/blog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.set('view engine', 'ejs');
 
-//MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const blogs = await Blog.find({});
+  res.render('index', {
+    blogs,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add_post');
+});
+app.post('/blog', async (req, res) => {
+  await Blog.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
---
+++ models/BlogDetails.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const BlogSchema = new Schema({
+    title: String,
+    detail: String,
+    dateCreated: {
+      type: Date,
+      default: Date.now,
+    },
+})
+
+const Blog = mongoose.model('Blog', BlogSchema)
+
+module.exports = Blog
